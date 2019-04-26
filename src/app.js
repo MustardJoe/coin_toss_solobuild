b5ff720bacef2bd921bd18eb2b7593fd31b8150c
Jonathan Forney <jonathan.forney@gmail.com>
2019-04-26 14:21:12 -0700

    have wired up buttons, coin now flips -next add counter to tally

--- src/app.js
+++ src/app.js
@@ -4,5 +4,13 @@ const coinImg = document.getElementById('coin-img');
 const doomicornButton = document.getElementById('doomicorn-button');
 
 doomicornButton.addEventListener('click', () => {
-        
+    const randomNumber = Math.random();
+    const flip = doomFlip(randomNumber);
+
+    const imageSource = './assets/pic' + flip + 'd3.jpg';
+    coinImg.src = imageSource;
+    console.log(flip);
+
+
+
 });
